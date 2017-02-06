9ac89f6d74317f38034910cf39d305753a5ea094
Ant Cosentino <antony@mycosentino.com>
2017-02-07 01:41:24 +0200

    test hardening

--- test/index.js
+++ test/index.js
@@ -3,9 +3,15 @@ var mw = require('../')
 
 describe('memorywaffle', function() {
   it('should invoke the start callback if provided when the interface is ready', function(done) {
-    mw({
-      foo: function(bar) {}
-    }, '$ ', done.bind(done, null))
+    try {
+      mw(
+        {foo: function(bar) {}},
+        '$ ',
+        done.bind(done, null)
+      )
+    } catch (e) {
+      done(e)
+    }
   })
 
   it('should throw if no commands have been given', function(done) {
@@ -14,25 +20,21 @@ describe('memorywaffle', function() {
     } catch (e) {
       return done()
     }
-    done(new Error('should not have been called')
+    done(new Error('should not have been called'))
   })
-  
+
     it('should throw if no parameters are defined in a function', function(done) {
     try {
-      mw({
-        foo: function() {}
-      })
+      mw({foo: function() {}})
     } catch (e) {
       return done()
     }
-    done(new Error('should not have been called')
+    done(new Error('should not have been called'))
   })
 
   it('should throw if the body of a function is not specified as a function', function(done) {
     try {
-      mw({
-        baz: 'qux'
-      })
+      mw({baz: 'qux'})
     } catch (e) {
       return done()
     }
